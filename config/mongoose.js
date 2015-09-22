35f741c0af2b4971f7a64d2a84b8a20003e475f6
Boshika Tara <boshikatara@gmail.com>
2015-09-22 13:10:57 -0700

    Persisting issues with DB connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-mongoose.connect('mongodb://localhost:27017/MiniStore');
+mongoose.connect('mongodb://localhost/MiniStore');
 var models_path = __dirname + '/../server/models'
 fs.readdirSync(models_path).forEach(function(file) {
   if(file.indexOf('.js') > 0) {
